2252eaca644559d1066519cc7be39f459af1663e
Cristian <123cristianjose@gmail.com>
2025-05-14 21:56:19 -0500

    entidad biblioteca

---
+++ modelo/entidades/biblioteca.php
@@ -0,0 +1,48 @@
+<?php
+class biblioteca
+{
+    //propiedades
+    private $bibliotecaid;
+    private $aforo;
+    private $area;
+
+    //constructor
+    public function __construct($bibliotecaid, $aforo, $area)
+    {
+        $this->bibliotecaid = $bibliotecaid;
+        $this->aforo = $aforo;
+        $this->area = $area;
+    }
+
+    //get y set
+    public function getBibliotecaid ()
+    {
+        return $this->bibliotecaid;
+    }
+
+    public function setBibliotecaid ($bibliotecaid)
+    {
+        return $this->bibliotecaid;
+    }
+
+    public function getAforo ()
+    {
+        return $this->aforo;
+    }
+
+    public function setAforo($aforo)
+    {
+        return $this->aforo;
+    }
+
+    public function getArea ()
+    {
+        return $this->area;
+    }
+
+    public function setArea ($area)
+    {
+        return $this->area;
+    }
+}
+?>
