d1613e1b423708ec89312522e52fd9b107705289
Cristian <123cristianjose@gmail.com>
2025-05-15 19:28:15 -0500

    crudBiblioteca C.

---
+++ modelo/persistencia/ICrudBd.php
@@ -0,0 +1,17 @@
+<?php
+interface ICrudBd
+{
+    //metodos
+    public function consultarPorId ($id);
+
+    public function consultarTodo ();
+
+    public function insertar ($objeto);
+
+    public function eliminarPorId ($id);
+
+    public function modificar ($objeto);
+
+    public function contar ();
+}
+?>
